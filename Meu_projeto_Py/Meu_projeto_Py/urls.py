045821f618aadcacff51813a7460578abe8b8293
robertaNicolle <164383080+robertaNicolle@users.noreply.github.com>
2025-01-23 23:44:03 -0300

    Delete Meu_projeto_Py directory

--- Meu_projeto_Py/Meu_projeto_Py/urls.py
+++
@@ -1,9 +0,0 @@
-# Meu_projeto_Py/urls.py
-
-from django.contrib import admin
-from django.urls import path, include  # Não se esqueça de importar o include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('cotacoes.urls')),  # Inclui as URLs do app cotacoes
-]
--- Meu_projeto_Py/cotacoes/urls.py
+++
@@ -1,8 +0,0 @@
-# cotacoes/urls.py
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('cotacao/', views.cotacao_view, name='cotacao'),
-]
